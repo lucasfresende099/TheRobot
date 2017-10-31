197624c0399dadb57fa98c223025ae32524f1df9
Lucas Resende <lucasresendef@hotmail.com>
2017-10-31 11:48:23 -0200

    Die and Respawn

---
+++ Assets/Scripts/GM.cs
@@ -0,0 +1,58 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM : MonoBehaviour {
+
+	public static GM instance = null;
+
+	public float yMinLive = -10f;
+
+	public Transform spawnPoint;
+
+	public GameObject playerPrefab;
+
+	PlayerCtrl player;
+
+	public float TimeToRespawn = 2f;
+
+	void Awake(){
+		if (instance ==null){
+			instance = this;
+		}
+		else if (instance != this){
+			Destroy(gameObject);
+		}
+		DontDestroyOnLoad(gameObject);
+	}
+
+	
+	void Start () {
+		if (player == null){
+			RespawnPlayer();
+		}
+	}
+	
+
+	void Update () {
+		if (player == null){
+			GameObject obj = GameObject.FindGameObjectWithTag("Player");
+			if( obj != null) {
+				player = obj.GetComponent<PlayerCtrl>();
+			}
+		}
+		
+	}
+
+	public void RespawnPlayer(){
+		Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+}
+
+    public void KillPlayer(){
+		if (player != null){
+			Destroy(player.gameObject);
+			Invoke("RespawnPlayer", TimeToRespawn);
+		}
+	}
+}
